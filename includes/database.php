41c90d273575a72eb3b6b803af92a1150855474f
fnemeth10 <fnemeth15@icloud.com>
2023-04-12 18:44:24 -0400

    Updates

--- includes/database.php
+++ includes/database.php
@@ -2,9 +2,9 @@
 
 function getDB() {
     $db_host = "sql9.freemysqlhosting.net";
-	$db_name = "sql9604955";
-	$db_user = "sql9604955";
-	$db_pass = "hXgfF5mzzg";
+	$db_name = "";
+	$db_user = "";
+	$db_pass = "";
 	
 	$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 	if (mysqli_connect_error()){
