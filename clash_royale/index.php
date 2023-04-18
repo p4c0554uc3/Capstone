74a71963813540fa1c05648638e97028a340fb0b
fnemeth10 <fnemeth15@icloud.com>
2023-04-18 15:49:52 -0400

    My files

---
+++ clash_royale/index.php
@@ -0,0 +1,12 @@
+<?php
+    require 'includes/header.php'; 
+?>
+	<br><br><a href="clash.php">Clash Royale API Data</a>
+	<br><br><a href="gameSearch.php">Giant Bomb API Game Search</a>
+	<br><br><a href="favoriteGames.php">Giant Bomb API Favorite Games</a>
+	<br><br><a href="Jolade_Stage3.php">Valorant API Data</a>
+	<br><br><a href="capstoneserverstyle.php">Apex API Stats</a>
+	<br><br><a href="owl.php">Overwatch League API Data</a>
+ <?php 
+    require 'includes/footer.php'; 
+?>
