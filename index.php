18b953a32c4c0c4bf9457887d89b495b162c7452
Emmanuel Jolade <emmanueljolade@gmail.com>
2023-04-19 18:03:12 -0400

    Fixed links for pages to connect on index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     require 'includes/header.php'; 
 ?>
-	<br><br><a href="view.php">Clash Royale API Data</a>
+	<br><br><a href="clash_royale/clash.php">Clash Royale API Data</a>
 	<br><br><a href="giantbombapi/gameSearch.php">Giantbomb API Add Game</a>
 	<br><br><a href="giantbombapi/favoriteGames.php">Giantbomb API Favorite Games List</a>
 	<br><br><a href="Valorant/Jolade_Stage3.php">Valorant API Data</a>
