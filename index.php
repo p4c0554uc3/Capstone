7caf22f776e7afeff0780c5e1a0008b56e0f1af2
Emmanuel Jolade <emmanueljolade@gmail.com>
2023-04-20 13:29:44 -0400

    Updated link to make Apex file connection work

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<br><br><a href="giantbombapi/gameSearch.php">Giantbomb API Add Game</a>
 	<br><br><a href="giantbombapi/favoriteGames.php">Giantbomb API Favorite Games List</a>
 	<br><br><a href="Valorant/Jolade_Stage3.php">Top 100 Valorant NA</a>
-	<br><br><a href="capstoneserverstyle.php">Apex API Data</a>
+	<br><br><a href="capstonewebseverstyle.php">Apex API Data</a>
 	<br><br><a href="owl.php">Overwatch League API Data</a>
  <?php 
     require 'includes/footer.php'; 
