8e9e982f12ce4b9bbf2568e75f7982cd2088ac6b
Emmanuel Jolade <emmanueljolade@gmail.com>
2023-04-20 19:20:32 -0400

    Updated Home Page

--- index.php
+++ index.php
@@ -1,12 +1,40 @@
 <?php
     require 'includes/header.php'; 
 ?>
-	<br><br><a href="clash_royale/clash.php">Clash Royale API Data</a>
-	<br><br><a href="giantbombapi/gameSearch.php">Giantbomb API Add Game</a>
-	<br><br><a href="giantbombapi/favoriteGames.php">Giantbomb API Favorite Games List</a>
-	<br><br><a href="Valorant/Jolade_Stage3.php">Top 100 Valorant NA</a>
-	<br><br><a href="capstonewebseverstyle.php">Apex API Data</a>
-	<br><br><a href="owl.php">Overwatch League API Data</a>
+<!DOCTYPE html>
+<html>
+	<head>
+		<style>
+		*{ font-family: Helvetica;}
+		
+		header{
+			text-align: center;
+			position: relative;
+			color: black;
+		}
+		
+		body{
+			background-image: url("includes/IndexBackground.jpg");
+			font-weight: bold;
+		}
+		
+		a:visited {
+			text-decoration: none;
+			color: black;
+		}
+		</style>
+	</head>
+	
+	<body>
+		<br><br>
+		<br><br><a href="clash_royale/clash.php">Clash Royale API Data</a>
+		<br><br><a href="giantbombapi/gameSearch.php">Giantbomb API Add Game</a>
+		<br><br><a href="giantbombapi/favoriteGames.php">Giantbomb API Favorite Games List</a>
+		<br><br><a href="Valorant/Jolade_Stage3.php">Top 100 Valorant NA</a>
+		<br><br><a href="Apex/capstoneseverstyle.php">Apex API Data</a>
+		<br><br><a href="owl.php">Overwatch League API Data</a>
+	</body>
+</html>
  <?php 
     require 'includes/footer.php'; 
 ?>
