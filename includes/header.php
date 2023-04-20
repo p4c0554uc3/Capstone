30348020e2e9f6569ab6fe6555d534b5e4605ba3
Emmanuel Jolade <emmanueljolade@gmail.com>
2023-04-20 19:20:58 -0400

    Updated Home Page

--- includes/header.php
+++ includes/header.php
@@ -4,12 +4,15 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Capstone Group 6</title>
+	<style>
+
+	</style>
+	<title>Ylisse's Game Vault</title>
 </head>
 <body>
 
 	<header>
-		<h1>Video Game API Project</h1>
+		<h1>Ylisse's Game Vault</h1>
 	</header>
 
   <main>
