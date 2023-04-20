614509d73e4d773ca8731701a5b2c4ebefb32d73
Emmanuel Jolade <emmanueljolade@gmail.com>
2023-04-19 20:19:13 -0400

    Edited my link to Valorant data

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<br><br><a href="clash_royale/clash.php">Clash Royale API Data</a>
 	<br><br><a href="giantbombapi/gameSearch.php">Giantbomb API Add Game</a>
 	<br><br><a href="giantbombapi/favoriteGames.php">Giantbomb API Favorite Games List</a>
-	<br><br><a href="Valorant/Jolade_Stage3.php">Valorant API Data</a>
+	<br><br><a href="Valorant/Jolade_Stage3.php">Top 100 Valorant NA</a>
 	<br><br><a href="capstoneserverstyle.php">Apex API Data</a>
 	<br><br><a href="owl.php">Overwatch League API Data</a>
  <?php 
